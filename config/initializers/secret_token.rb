4408a97ffe6cd495ae053dd670621442a7094d43
DarryQueen <slimfrog@gmail.com>
2015-08-26 16:43:17 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SplitServer::Application.config.secret_token = 'dd2d4430d5b0375adcf723f77c18b596e8093528e60f13c8a6bdf16c6c29b2a47cf2e4a68d508b07d5e94907f895d02c68009e2073f7f39ba1cf7521b35f25a3'
